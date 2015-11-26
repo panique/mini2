c82a1acfd1eb0d492e1103eb7f230d791548cf94
dulltz <isrgnoe@gmail.com>
2015-11-26 18:28:06 +0900

    マイグレーションのためのサンプルコードを追加

---
+++ Mini/_install/migrate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# vagrant up によってprovisioningを行った後、データベースを更新したい時のためのシェルスクリプト
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='12345678'
+PROJECTFOLDER='myproject'
+
+
+sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/Mini/_install/04-insert-demo-data-into-table-song.sql"
+
+
